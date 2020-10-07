2b587b8ded9f19588f669efd6f9f1e970eddb08b
LiliaRibac <ribac.lilia@gmai.com>
2020-10-07 16:53:13 -0700

    add handle submit fundtion

--- budget-calculator/src/App.js
+++ budget-calculator/src/App.js
@@ -25,6 +25,13 @@ function App() {
 
   // alert
   const [alert, setAlert] = useState({ show: false });
+
+  // edit
+  const [edit, setEdit] = useState(false);
+
+  // edit item
+  const [id, setId] = useState(0);
+
   // functionality
 
   // handle charge
@@ -49,9 +56,19 @@ function App() {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (charge !== '' && amount > 0) {
-      const singleExpense = { id: uuidv4(), charge, amount };
-      setExpenses([...expenses, singleExpense]);
-      handleAlert({ type: 'success', text: 'item added' });
+      if (edit) {
+        let tempExpenses = expenses.map((item) => {
+          return item.id === id ? { ...item, charge, amount } : item;
+        });
+        setExpenses(tempExpenses);
+        setEdit(false);
+        handleAlert({ type: 'success', text: 'item edited' });
+      } else {
+        const singleExpense = { id: uuidv4(), charge, amount };
+        setExpenses([...expenses, singleExpense]);
+        handleAlert({ type: 'success', text: 'item added' });
+      }
+
       setCharge('');
       setAmount('');
     } else {
@@ -79,7 +96,12 @@ function App() {
 
   // handle edit
   const handleEdit = (id) => {
-    console.log(`item edited :${id}`);
+    let expenseItems = expenses.find((item) => item.id === id);
+    let { charge, amount } = expenseItems;
+    setCharge(charge);
+    setAmount(amount);
+    setEdit(true);
+    setId(id);
   };
 
   return (
@@ -94,6 +116,7 @@ function App() {
           handleCharge={handleCharge}
           handleAmount={handleAmount}
           handleSubmit={handleSubmit}
+          edit={edit}
         />
         <List
           expenses={expenses}
